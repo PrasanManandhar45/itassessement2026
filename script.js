7cbbedb0e8838ffd2232ceab55046c6a358ee9f0
PrasanManandhar45 <prasanmanandhar5@gmail.com>
2026-04-18 21:12:26 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function openImage(element) {
+  const img = element.querySelector("img");
+  document.getElementById("popup").style.display = "flex";
+  document.getElementById("popupImg").src = img.src;
+}
+
+function closeImage() {
+  document.getElementById("popup").style.display = "none";
+}function openImage(element) {
+  const img = element.querySelector("img");
+  document.getElementById("popup").style.display = "flex";
+  document.getElementById("popupImg").src = img.src;
+}
+
+function closeImage() {
+  document.getElementById("popup").style.display = "none";
+}
